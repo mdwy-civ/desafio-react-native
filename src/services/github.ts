79fd589529c824a5ee084c957de901bd8c716f42
Rodrigo Lourenço <rsilvape@gmail.com>
2021-02-05 11:34:05 -0300

    Adicionando o token

--- src/services/github.ts
+++ src/services/github.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export function getUser(usuario: string) {
+  var token = '422a1461d0b5d946813dcc85eeaf9110fa95b7bb';
   return axios.get('https://api.github.com/users/' + usuario, {
     headers: {
-      Authorization: 'Bearer 422a1461d0b5d946813dcc85eeaf9110fa95b7bb',
+      Authorization: 'Bearer ' + token,
     },
   });
 }
